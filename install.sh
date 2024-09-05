659c1b8f3b76981a903461bfd9da62d22798d393
Cameron Candau <camcandau@gmail.com>
2024-09-05 16:56:29 -0700

    integrated fonts installation script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-mkdir ansible-setup && cd ansible-setup
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install ansible-core -y
+mkdir ansible-setup && cd ansible-setup
 git clone https://github.com/Cam-Can-Do/ansible-personal.git
 cd ansible-personal
 ansible-playbook -i hosts.ini main.yml -K
