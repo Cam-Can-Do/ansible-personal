7aedd8682b9cf2a440bf3ae4bd356165209d6147
Cameron Candau <camcandau@gmail.com>
2025-08-10 11:24:32 -0700

    updated and adapted for kali vm setup

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ sudo apt-get install ansible-core -y
 mkdir ansible-setup && cd ansible-setup
 git clone https://github.com/Cam-Can-Do/ansible-personal.git
 cd ansible-personal
-ansible-playbook -i hosts.ini main.yml -K
+ansible-playbook -i hosts.ini kali_vm.yml -K
